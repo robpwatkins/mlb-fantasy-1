e53b4b259755163c12a646c2aef9a5c248f4f329
robpwatkins <robpwatkins@gmail.com>
2021-04-11 10:36:17 -0500

    well that was easy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
