70cb1506a3811bbff3f4d7d1ad8df5a2b29ba1c1
robpwatkins <robpwatkins@gmail.com>
2021-04-14 21:18:44 -0500

    checking

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Profile() {
   const { user, isAuthenticated } = useAuth0();
+
+  useEffect(() => {
+    console.log(isAuthenticated);
+  }, [])
 
   return (
     isAuthenticated && (
