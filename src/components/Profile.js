8e3a7df6bd6d3288f3739a2488ab77b3becfbb1e
robpwatkins <robpwatkins@gmail.com>
2021-04-14 21:32:55 -0500

    not gonna mess with cookies right now

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,10 +4,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 export default function Profile() {
   const { user, isAuthenticated } = useAuth0();
 
-  useEffect(() => {
-    console.log(isAuthenticated);
-  }, [])
-
   return (
     isAuthenticated && (
       <div>
