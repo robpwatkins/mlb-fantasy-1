888ab184fd521963e643ea72e851e3a5ef50c7ab
robpwatkins <robpwatkins@gmail.com>
2021-04-14 21:12:51 -0500

    got a counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <br/>
       <LoginButton />
       <LogoutButton />
       <Profile />
