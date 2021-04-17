f196949757ebacb0df8fb7d471e7f26dfe8388a2
robpwatkins <robpwatkins@gmail.com>
2021-04-16 21:26:30 -0500

    starting from scratch w/ fauna

--- src/api/getCount.js
+++
@@ -1,9 +0,0 @@
-import { client, q } from '../config/db'
-
-const getCount = client.query(
-  q.Paginate(
-    q.Match(
-      q.Ref('indexes/all')
-    )
-  )
-)
--- src/config/db.js
+++
@@ -1,6 +0,0 @@
-import faunadb from 'faunadb'
-
-const client = new faunadb.Client({ secret: process.REACT_APP_FAUNADB_KEY });
-const q = faunadb.query;
-
-export { client, q }
