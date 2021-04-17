1769ca697da49162a3f4727b729df0d5e57634d3
robpwatkins <robpwatkins@gmail.com>
2021-04-17 11:01:59 -0500

    debugging 'faunadb.query is not a function'

--- functions/getCount/getCount.js
+++ functions/getCount/getCount.js
@@ -3,22 +3,25 @@ const faunadb = require('faunadb')
 
 const faunaClient = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
 
+const q = faunadb.query
+
 const handler = async (event) => {
   if (event.httpMethod !== 'GET') {
     return { statusCode: 500, body: 'GET OUTTA HERE!' }
   }
 
   try {
-    // const req = await faunadb.query(q.Map(q.Paginate(q.Match(q.Index("count")))))
+    const req = await faunadb.query(
+      q.Map(
+        q.Paginate(
+          q.Match(
+            q.Index("all_counters"))),  q.Lambda("attr", q.Get(q.Var("attr")))))
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Helloooo you` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ body: JSON.stringify(req.data) }),
     }
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) }
   }
 }
 
