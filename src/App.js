9e4207bc6e16db6725f99b0805e233be82e8a353
robpwatkins <robpwatkins@gmail.com>
2021-04-20 22:52:07 -0500

    getting email from the auth0 object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
@@ -8,42 +9,51 @@ function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [highScore, setHighScore] = useState();
   const [currentPlayer, setCurrentPlayer] = useState({});
+  const { user, isAuthenticated } = useAuth0();
 
   const getCurrentPlayer = async () => {
-    const resp = await fetch('/api/getPlayers');
-    const [player] = await resp.json();
-    console.log(player.data.high_score);
-    setCurrentPlayer(player);
-    setHighScore(player.data.high_score);
-  }
-
-  const updateHighScore = async (newScore) => {
-    console.log(currentPlayer);
-    fetch('/api/updatePlayer', {
-      method: 'PATCH',
-      body: JSON.stringify({
-        currentPlayer, newScore
-      })
-    })
-  }
-
-  const handleClick = () => {
-    setCurrentScore(currentScore + 1);
-  }
-
-  useEffect(() => {
-    getCurrentPlayer();
-  }, [])
-
-  useEffect(() => {
-    console.log(currentScore, highScore);
-    if (currentScore > highScore) {
-      // let newScore = currentScore;
-      updateHighScore(currentScore);
-      setHighScore(currentScore);
+    const resp = await fetch('/api/getPlayers', {
+      // method: 'GET',
+      // body: JSON.stringify({
+        //   // email: 
+        // })
+      });
+      const [player] = await resp.json();
+      setCurrentPlayer(player);
+      setHighScore(player.data.high_score);
     }
-  }, [currentScore])
+    
+    const updateHighScore = async (newScore) => {
+      console.log(currentPlayer);
+      fetch('/api/updatePlayer', {
+        method: 'PATCH',
+        body: JSON.stringify({
+          currentPlayer, newScore
+        })
+      })
+    }
+    
+    const handleClick = () => {
+      setCurrentScore(currentScore + 1);
+    }
+    
+    useEffect(() => {
+      getCurrentPlayer();
+    }, [])
+    
+    useEffect(() => {
+      console.log(currentScore, highScore);
+      if (currentScore > highScore) {
+        // let newScore = currentScore;
+        updateHighScore(currentScore);
+        setHighScore(currentScore);
+      }
+    }, [currentScore])
 
+    useEffect(() => {
+      isAuthenticated && console.log(user.email);
+    }, [isAuthenticated])
+    
   return (
     <div>
       <p>High score: {currentPlayer.data && currentPlayer.data.high_score}</p>
