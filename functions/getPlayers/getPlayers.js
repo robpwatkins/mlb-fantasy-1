23ae2b686888f9a758cb701e3e5a58dae7651aca
robpwatkins <robpwatkins@gmail.com>
2021-04-20 22:11:22 -0500

    getting closer

--- functions/getPlayers/getPlayers.js
+++ functions/getPlayers/getPlayers.js
@@ -14,7 +14,7 @@ const handler = async (event) => {
       q.Map(
         q.Paginate(
           q.Match(
-            q.Index("all_players_by_email"), "test@tester.com")), q.Lambda("attr", q.Get(q.Var("attr")))))
+            q.Index("all_players"))), q.Lambda("X", q.Get(q.Var("X")))))
             console.log(JSON.stringify(req.data));
     return { statusCode: 200, body: JSON.stringify(req.data) }
   } catch (error) {
