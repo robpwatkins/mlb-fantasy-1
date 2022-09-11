ff62ca8d127ab5acd7263a58e71c848682b11689
robpwatkins <robpwatkins@gmail.com>
2022-09-11 09:35:02 -0500

    updating to createRoot

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientid = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <Auth0Provider
     domain={domain}
     clientId={clientid}
     redirectUri={window.location.origin}
   >
     <App />
-  </Auth0Provider>,
-  document.getElementById('root')
+  </Auth0Provider>
 );
